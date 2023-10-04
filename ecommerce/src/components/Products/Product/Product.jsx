da2a11ccc970cd4cd80c08bf0e66a5f19cb9570e
waliray123 <willy2000.wld@gmail.com>
2023-10-03 18:17:08 -0600

    Productos quemados agregados

---
+++ ecommerce/src/components/Products/Product/Product.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Card,CardMedia,CardContent,CardActions,Typography,IconButton } from '@mui/material'
+import { AddShoppingCart } from '@mui/icons-material'
+
+import useStyles from './styles'
+
+function Product({producto}) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+        <CardMedia className={classes.media} image={producto.imagen} title={producto.nombre}></CardMedia>
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography variant='h5' gutterBottom>
+                    {producto.nombre}
+                </Typography>
+                <Typography variant='h5'>
+                    Q.{producto.precio}
+                </Typography>
+
+            </div>
+            <Typography variant='h2' color='textSecondary'>
+                {producto.descripcion}
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label='Add to Cart'>
+                <AddShoppingCart/>
+            </IconButton>
+        </CardActions>
+
+    </Card>
+
+  )
+}
+
+export default Product
---
+++ ecommerce/src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Grid } from '@mui/material'
+import Product from "./Product/Product";
+
+const productos = [
+    {id: 1, nombre: 'Computadora', descripcion: 'Laptop nueva del mercado', precio: '100', imagen: 'https://media.wired.com/photos/64daad6b4a854832b16fd3bc/master/w_1920,c_limit/How-to-Choose-a-Laptop-August-2023-Gear.jpg'},
+    {id: 2, nombre: 'Laptop vieja', descripcion: 'Laptop vieja del mercado', precio: '50', imagen: 'https://www.asus.com/media/Odin/Websites/global/ProductLine/20201014051549/P_setting_xxx_0_90_end_185.png?webp'},
+]
+
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {productos.map((producto) => (
+                    <Grid item key={producto.id} xs={12} sm={6} lg={3}>
+                        <Product producto={producto}></Product>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    );
+}
+
+export default Products;
