c5aa4fb1320c9f5f11e3586490e25c43752b339b
waliray123 <willy2000.wld@gmail.com>
2023-10-08 11:15:56 -0600

    Filtro de productos agregado

--- ecommerce/src/components/Products/Products.jsx
+++ ecommerce/src/components/Products/Products.jsx
@@ -1,22 +1,44 @@
-import React from "react";
-import { Grid } from '@mui/material'
+import React, { useState } from "react";
+import { Grid,TextField,Box } from '@mui/material'
 import Product from "./Product/Product";
 import { useStyles } from "./styles";
 
-const productos = [
+const productosTraidos = [
     {id: 1, nombre: 'Computadora', descripcion: 'Laptop nueva del mercado', precio: '100', imagen: 'https://media.wired.com/photos/64daad6b4a854832b16fd3bc/master/w_1920,c_limit/How-to-Choose-a-Laptop-August-2023-Gear.jpg'},
     {id: 2, nombre: 'Laptop vieja', descripcion: 'Laptop vieja del mercado', precio: '50', imagen: 'https://www.asus.com/media/Odin/Websites/global/ProductLine/20201014051549/P_setting_xxx_0_90_end_185.png?webp'},
 ]
 
 
 
+
 const Products = () => {    
     const classes = useStyles();
+    const [filtro, setFiltro] = useState('');
+    const [productos, setProductos] = useState(productosTraidos);
+
+    const handleFiltroChange = (event) => {
+        setFiltro(event.target.value);
+    };
+  
+    const productosFiltrados = productos.filter((producto) =>
+        producto.nombre.toLowerCase().includes(filtro.toLowerCase())
+    );
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
+            <Box m={2}>
+                <TextField
+                    label="Filtrar por título"
+                    variant="outlined"
+                    fullWidth
+                    value={filtro}
+                    onChange={handleFiltroChange}
+                />
+                
+            </Box>
+            
             <Grid container justify="center" spacing={4}>
-                {productos.map((producto) => (
+                {productosFiltrados.map((producto) => (
                     <Grid item key={producto.id} xs={12} sm={6} lg={3}>
                         <Product producto={producto}></Product>
                     </Grid>
