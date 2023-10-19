6e2ce6aed80f62185a8e1833332c5ef6dbcf3c5a
waliray123 <willy2000.wld@gmail.com>
2023-10-19 15:21:25 -0600

    Ruta de busqueda a un producto por id agregado

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -22,11 +22,37 @@ const getAllProducts = async (req, res) => {
     }
 }
 
+const getProductById = async (req, res) => {
+    const productId = req.params.id;
+    const conn = await db.getConnection();
+    try {
+        const product = await conn.query('SELECT p.*,s.supplier_name FROM PRODUCT AS p INNER JOIN SUPPLIER AS s ON p.supplier_id=s.supplier_id WHERE p.product_id = ?', [productId]);
+        
+        if (product.length === 1) {
+          product[0].tags = await getTags(conn, product[0]);
+          product[0].images = await getImages(conn, product[0]);
+          res.json(product[0]);
+        } else {
+          res.status(404).json({
+            message: "Producto no encontrado"
+          });
+        }
+    } catch (error) {
+        console.error(error);
+        res.json({
+          message: "Ocurrió un error al obtener el producto"
+        });
+    } finally {
+        if (conn) conn.end();
+    }
+}
+
 const insertProduct = async (req, res) => {
     
 }
 
 export {
     getAllProducts,
+    getProductById,
     insertProduct
 }
--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -4,5 +4,6 @@ import * as ProductsController from '../controllers/productsController.js';
 const router = Router();
 
 router.get('/getAllProducts', ProductsController.getAllProducts);
+router.get('/getProduct/:id', ProductsController.getProductById);
 
 export { router as ProductsRoutes }
