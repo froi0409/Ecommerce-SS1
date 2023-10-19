37b7e574372cdfceb8c762d7cb12f0f22ae088c9
froi0409 <ferrubocana@gmail.com>
2023-10-19 04:33:45 -0600

    Ahora se envían las imágenes en la descripción de los productos

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -4,22 +4,22 @@ import { getTags, getImages } from '../services/productManagement/productPropert
 
 const getAllProducts = async (req, res) => {
     const conn = await db.getConnection();
-
+    let productList = {};
     try {
-        const productList = await conn.query('SELECT p.*,s.supplier_name FROM PRODUCT AS p INNER JOIN SUPPLIER AS s ON p.supplier_id=s.supplier_id');
+        productList = await conn.query('SELECT p.*,s.supplier_name FROM PRODUCT AS p INNER JOIN SUPPLIER AS s ON p.supplier_id=s.supplier_id');
         for (const product of productList) {
             product.tags = await getTags(conn, product); //get product tags
-            await getImages(conn, product);
+            product.images = await getImages(conn, product);
         }
-        console.log(productList);
+        res.json(productList);
     } catch (error) {
         console.error(error);
+        res.json({
+            message: "Ocurrió un error al obtener los productos"
+        })
     } finally {
         if (conn) conn.end();
     }
-    res.json({
-        message: 'hola mundo'
-    });
 }
 
 const insertProduct = async (req, res) => {
--- backend/src/services/productManagement/productProperties.js
+++ backend/src/services/productManagement/productProperties.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 export async function getTags(connection, product) {
     const tagsObject = await connection.query('SELECT category_name FROM PRODUCT_CATEGORY WHERE product_id=?', [ product.product_id ]); // get all product categories 
     const tags = [];
@@ -7,15 +9,39 @@ export async function getTags(connection, product) {
     return tags;
 }
 
-export async function getImages(connection, product) {
-    const imagesObject = await connection.query('SELECT image_path FROM PRODUCT_IMAGE WHERE product_id=?', [ product.product_id ]);
-    const images = [];
-    for (const image of imagesObject) {
-        const imagePath = 'public/images/' + image.image_path;
-        console.log(imagePath);
-    }
-}
+export function getImages(connection, product) {
+    return new Promise(async (resolve, reject) => {
+        const imagesObject = await connection.query('SELECT image_path FROM PRODUCT_IMAGE WHERE product_id=?', [product.product_id]);
+        const images = [];
 
+        const readImage = (image) => {
+            return new Promise((resolve, reject) => {
+                const imagePath = 'public/images/' + image.image_path;
+
+                fs.readFile(imagePath, (err, data) => {
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        console.log(image.image_path);
+                        const imageBase64 = data.toString('base64');
+                        images.push(imageBase64);
+                        resolve();
+                    }
+                });
+            });
+        };
+
+        const promises = imagesObject.map((image) => readImage(image));
+
+        try {
+            await Promise.all(promises);
+            resolve(images);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
 function getFileExtension(filename) {
     return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
 }
