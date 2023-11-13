453f7bc73a2fd7c5a00eb412b1bea129646d7110
froi0409 <ferrubocana@gmail.com>
2023-11-13 12:48:26 -0600

    Se agregó la lógica que manejará las transacciones de interoperabilidad

--- backend/src/services/other/paymentPortalManagement.js
+++ backend/src/services/other/paymentPortalManagement.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv'
 import axios from 'axios';
 
+config();
+
 export async function validateAccount(account, associationToken) {
     try {
         const postData = {
@@ -24,10 +26,34 @@ export async function validateAccount(account, associationToken) {
 
 export async function validateMonetaryTransaction(sourceAccount, destinationAccount, amount) {
     try {
+        const postData = {
+            sourceAccount: sourceAccount,
+            destinationAccount: destinationAccount,
+            amount: amount
+        }
+
+        // Esto lo tendremos que eliminar cuando ya estén habilitados los usuarios
         return {
             status: true, // si la transacción no fue exitosa deberá retornar false
             message: 'Validación exitosa'
         };
+
+        const response = await axios.post(`${process.env.PAYMENT_PORTAL_URL}/transaction`, postData)
+        const status = response.data.status;
+        const message = response.data.message;
+        console.log(`Validación de Transacción: ${status}`);
+        
+        if (status === undefined) {
+            throw new Error('Error de Servidor de Portal de Pagos');
+        } else if (status === 100) {
+            return {
+                status: true, // si la transacción no fue exitosa deberá retornar false
+                message: 'Validación exitosa'
+            }
+        } else {
+            throw new Error(message);
+        }
+        
     } catch (error) {
         throw error;
     }
